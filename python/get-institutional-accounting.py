0194947110faf75ee3fb96909cfdef38423e89a5
Owain Kenway <o.kenway@ucl.ac.uk>
2017-08-03 17:16:19 +0100

    Making the mess worse.

--- python/get-institutional-accounting.py
+++ python/get-institutional-accounting.py
@@ -1,15 +1,4 @@
 #!/usr/bin/env python3
-''' 
-    This is a fundamentally terrible piece of programming.
-    If this is still in use in 2018 something has gone wrong.
-
-    Anyway, this is a main script that calls out to other things that:
-      1. Gets the amount of credit charged in Gold.
-      2. Gets the number of CPU hours recorded in the thomas SGE logs DB
-      3. Uses these to estimate ratio of paid to unpaid usage for a given month.
-
-    Dates are a list of ints [year, month] for reasons.
-'''
 
 '''
     Have a standardised way of incrementing months, unlike that *other*
@@ -26,6 +15,37 @@ def incrementmonth(orig, inc=1):
     ny = ny + 1
 
   return [ny, nm]
+
+'''
+    Build an SQL list out of a python list.
+'''
+def sqllist(pylist):
+    sqlstr="("
+    if type(pylist) == str:
+        sqlstr = sqlstr + "'" + pylist + "')"
+    else:
+        for a in pylist:
+            if sqlstr!= "(":
+                sqlstr = sqlstr + ", "
+            sqlstr = sqlstr + "'" + a + "'"
+        sqlstr = sqlstr + ")"
+    return sqlstr
+
+
+'''  
+    Build owner/node limit string for queries.
+'''
+def onlimits(users="*"):
+    query = ""
+
+    # if users != * then construct a node list.
+    if users != "*":
+        userlist = sqllist(users)
+        query = query + " and owner in " + userlist
+
+    return query
+
+
 
 ''' 
     Convert a [year, month] month into a string that can be passed to Gold.
@@ -48,10 +68,10 @@ def mysqlmonth(month):
     Call out to an external shell script to get Gold usage.  This is because
     we don't really have a view of the Gold DB.
 '''
-def getgoldusage(month):
+def getgoldusage(month, institute):
   import subprocess
   endmonth = incrementmonth(month)
-  gargs = ["./shell/get-gold-usage", goldmonth(month),  goldmonth(endmonth)]
+  gargs = ["./shell/get-gold-usage-inst", goldmonth(month),  goldmonth(endmonth), institute]
   usage = float(subprocess.check_output(gargs))
   return usage
 
@@ -92,25 +112,48 @@ def getmysqldata(query):
     This procedure uses my simpletemplate library to construct a query from a
     separate .sql file and then passes it to getmysqldata.
 '''
-def getmysqlusage(month):
+def getmysqlusage(month, users='*'):
   import simpletemplate as st
 
   monthstr = mysqlmonth(month)
   emonthstr = mysqlmonth(incrementmonth(month))
   
   # These are the keys in the file that will be replaced.
-  keys = {'%DB%':'thomas_sgelogs', '%START%':monthstr, '%STOP%':emonthstr}
-  query = st.templatefile(filename="sql/usage.sql", keys=keys)
+  keys = {'%DB%':'thomas_sgelogs', '%START%':monthstr, '%STOP%':emonthstr '%ONLIMITS%':onlimits(users=users)}
+  query = st.templatefile(filename="sql/usage-opts.sql", keys=keys)
 
   # The data is returned as a tuple inside a list for reasons.  Yank it out and
   # convert to hours from seconds.
   output = float(getmysqldata(query)[0][0])/3600.0
   return output
-  
+
+'''
+    Get a list of users for a given institute.
+'''
+def getusers(institute):
+  import simpletemplate as st
+
+  keys = {'%INSTITUTE%':institute}
+  query = st.templatefile(filename="sql/ist-to-users.sql", keys=keys)
+  output = getmysqldata(query)
+  print(output)  
+  return ['uccaoke']
+
+'''
+    Get a list of institutions from Heather's DB.
+'''
+def getinstitutes():
+  query = "select inst_id from thomas.institutes"
+  output = getmysqldata(query)
+  print(output)
+  return ['UCL']
+
+
 '''
     Our "not actually a main procedure because this is python". 
     Syntax is very simple - single argument each for year, month.
 '''
+
 def main():
   import sys
 
@@ -120,17 +163,12 @@ def main():
 
   month = [int(sys.argv[1]), int(sys.argv[2])]
 
-  gusage = getgoldusage(month)
-  print("Paid usage (hours): %.2f" % gusage)
-  musage = getmysqlusage(month)
-  print("SGE usage (hours): %.2f" % musage)
-
-  percentpaid=100.0*(gusage/musage)
-  percentfree=100.0-percentpaid
-
-  print("")
-  print("%% Paid: %.2f" % percentpaid)
-  print("%% Free: %.2f" % percentfree)  
+  # Get a list of institutes.
+  institutes = getinstitutes()
+  for a in institutes:
+    users = getusers(a)
+#    usage = getmysqlusage(month, users)
+#    print(a + " SGE usage: " + str(usage))
 
 '''
     *sigh*
